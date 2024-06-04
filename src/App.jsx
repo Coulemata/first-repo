b6789ffa83b2a86fadc6b2443cbb0d13d683897b
Coulemata <coulemata.traore@gmail.com>
2024-06-04 14:59:20 +0200

    firstt

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h2>Emission TV</h2>
+    <div><img src="" alt="" /></div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/Title.jsx
@@ -0,0 +1,9 @@
+import App from "../../App.json";
+
+
+const Title = (props) => {
+    return <h2>{props.category}</h2>;
+  };
+  
+  export default Title;
+  
